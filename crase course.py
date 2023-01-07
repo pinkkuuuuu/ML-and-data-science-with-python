d9c3fee025c2da89214112100c1de873bf027b14
Pinkesh Tripathi <115294297+pinkkuuuuu@users.noreply.github.com>
2023-01-07 14:44:18 +0530

    Create crase course.py

---
+++ crase course.py
@@ -0,0 +1,99 @@
+
+2
+1*3
+​
+3
+1/2
+0.5
+2 ** 4
+16
+2+3 * 5+5
+22
+(2*5) + (45-34)
+21
+4%2
+0
+5%2
+​
+1
+var= 3
+var
+3
+x=2
+y=3
+x+y
+5
+x=x+x
+x
+4
+#strings
+ x='hello'
+x    
+'hello'
+print(x)
+hello
+num = 64
+name ='pinkku'
+'my number is {} and my name is {}'.format(num,name)
+'my number is 64 and my name is pinkku'
+print('my number is {} and my name is {}'.format(num,name))
+my number is 64 and my name is pinkku
+print('my number is {one} and my name is {two}'.format(one=num,two=name))
+my number is 64 and my name is pinkku
+print('my number is {one} and my name is {two},and {two}'.format(one=num,two=name))
+my number is 64 and my name is pinkku,and pinkku
+#index strings
+s = 'hello'
+s[0]
+'h'
+s[3]
+'l'
+p = 'abcdefghi'
+p[0]
+'a'
+p[0:]
+'abcdefghi'
+p[:5]
+'abcde'
+p[5:8]
+'fgh'
+#list
+[1,2,3]
+[1, 2, 3]
+['a','b','c']
+['a', 'b', 'c']
+my_list = ['a','b','c']
+my_list
+['a', 'b', 'c']
+my_list.append('d')
+my_list
+['a', 'b', 'c', 'd', 'd', 'd']
+my_list[0]
+'a'
+my_list[2:4]
+['c', 'd']
+my_list[4] = 'e'
+my_list
+['a', 'b', 'c', 'd', 'e', 'e']
+my_list[5]='f'
+my_list
+['a', 'b', 'c', 'd', 'e', 'f']
+nest = [1,2,3,4]
+nest
+[1, 2, 3, 4]
+nest = [1,2,[3,4]]
+nest
+[1, 2, [3, 4]]
+nest[1]
+2
+nest[2]
+[3, 4]
+nest[2][0]
+3
+nest = [1,2,3,[4,5,['pinku']]]
+nest[3]
+[4, 5, ['pinku']]
+nest[3][0]
+4
+print(nest[3][2][0])
+pinku
