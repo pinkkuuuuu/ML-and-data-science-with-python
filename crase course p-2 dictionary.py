ab654ae75294613d6a8669dc607598e1b1c45d1f
Pinkesh Tripathi <115294297+pinkkuuuuu@users.noreply.github.com>
2023-01-07 15:22:28 +0530

    Create crase course p-2 dictionary.py

---
+++ crase course p-2 dictionary.py
@@ -0,0 +1,95 @@
+d = {'key':'value','key2':123}
+
+d['key']
+
+d['key2']
+
+d = {'k1':[1,2,3]}
+
+my_list = d['k1']
+
+
+
+d = {'k1':{'innerkey':[1,2,3]}}
+
+d['k1']['innerkey'][1]
+
+True
+
+
+my_list =[1,2,3]
+
+my_list[0]
+
+t =(1,2,3)
+
+t[0]
+
+my_list[0] = 'new'
+
+my_list
+
+set([1,2,3,3,4,5,5,5,6,6,6,7,7])
+
+s ={1,2,3}
+
+s.add(4)
+
+s
+
+s.add(5)
+
+s
+
+#comparison operator
+
+1 > 2
+
+2 > 1
+
+1 <= 1
+
+1 == 1
+
+1 == 2
+
+1 != 3
+
+'hi'!= 'bye'
+
+#logic operator
+
+(1 < 2) and (2 > 3)
+
+(1<2) or (2>3) or (1==1)
+
+True or False
+
+#if else statements
+
+if 1 < 2:
+    
+    print('hello')
+
+if True:
+    print('pinku')
+
+if True:
+    x = 2+2
+
+x
+
+if 1 == 2:
+    print('first')
+else:
+    print('last')
+
+if 1 == 2:
+    print('first')
+elif 4 ==4:
+    print('second')
+elif 3==3:
+    print('middle')
+else:
+    print('last')
+
